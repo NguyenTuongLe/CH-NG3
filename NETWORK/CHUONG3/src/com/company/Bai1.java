96bde567dc741514985499d84537d70e4368465e
NguyenTuongLe <tuongle200220@gmail.com>
2020-09-17 21:34:23 +0700

    bai 123

---
+++ NETWORK/CHUONG3/src/com/company/Bai1.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Bai1 {
+
+    public static void main(String[] args) throws MalformedURLException {
+            Scanner nhap = new Scanner(System.in);
+            System.out.println("Nhap dia chi URL: ");//https://www.w3schools.com/
+            String name = nhap.nextLine();
+            URL url = new URL(name);
+            System.out.println("URL : " + url.toString());
+            System.out.println("file name : " + url.getFile());
+            System.out.println("host : " + url.getHost());
+            System.out.println("port : " + url.getPort());
+            System.out.println("protocol : " + url.getProtocol());
+        }
+    }
---
+++ NETWORK/CHUONG3/src/com/company/Bai2.java
@@ -0,0 +1,30 @@
+package com.company;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.*;
+import java.util.Scanner;
+
+public class Bai2 {
+
+    public static void main(String arg[ ]) throws IOException
+    {
+        int i;
+        InputStream a;
+        //Nhap URL
+        Scanner nhap = new Scanner(System.in);
+        System.out.print("Nhap dia chi URL :");
+        String url = nhap.nextLine();
+        try
+        {
+            URL u = new URL(url);//https://www.w3schools.com/
+            a = (InputStream)u.getContent();
+            while((i=a.read())>0)
+                System.out.print((char)i);
+            System.out.println();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
